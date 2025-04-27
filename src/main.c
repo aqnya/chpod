9267fec20ccf5d00bcc62b309e76b3613291782c
April8th <siyueba73@gmail.com>
2025-04-28 01:35:21 +0800

    renew main.

--- src/main.c
+++ src/main.c
@@ -1,98 +1,90 @@
-/*
- * Copyright (C) 2023-2024  April8th
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <https://www.gnu.org/licenses/>.
- */
-
 #include "include/chd.h"
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
-#include <limits.h>
-#include <sys/stat.h>
+// 命令处理函数声明
+void handle_install(int argc, char *argv[]);
+void handle_help(int argc, char *argv[]);
+void handle_run(int argc, char *argv[]);
 
-void dispatch_arguments(int argc, char *argv[]) {
-  // 检查是否有足够的参数
-  if (argc < 2) {
-    print_help(argv[0]);
-    return;
-  }
+// 命令选项结构体
+typedef struct {
+    const char *short_opt;
+    const char *long_opt;
+    void (*handler)(int argc, char *argv[]);
+    const char *description;
+} CommandOption;
 
-  // 检查选项类型
-  const char *option = argv[1];
-  if (option[0] == '-') {
-    // 短选项或长选项
-    if (option[1] == '-') {
-      // 长选项
-      if (strcmp(option + 2, "install") == 0) {
-        if (argc >= 4) {
-          pull(argv[2], argv[3]);
-        } else {
-          cprintf(RED, "Error: insufficient arguments for --install\n");
-          print_help(argv[0]);
-        }
-      } else if (strcmp(option + 2, "help") == 0) {
-        print_help(argv[0]);
-      } else if (strcmp(option + 2, "run") == 0) {
-        run_proot_container(argv[2]);
-      } else if (strcmp(option + 2, "test") == 0) {
-        // 测试选项的处理
-      } else {
-        cprintf(RED, "Error: unknown option \"%s\"\n", option);
-        print_help(argv[0]);
-      }
+// 命令注册表
+CommandOption options[] = {
+    {"-i", "--install", handle_install, "Install rootfs."},
+    {"-h", "--help", handle_help, "Show help information."},
+    {"-r", "--run", handle_run, "Run container with proot."},
+    {NULL, NULL, NULL, NULL}
+};
+
+// 处理安装命令
+void handle_install(int argc, char *argv[]) {
+    if (argc >= 4) {
+        pull(argv[2], argv[3]);
     } else {
-      // 短选项
-      switch (option[1]) {
-      case 'i': // -i or --install
-        if (argc >= 4) {
-          pull(argv[2], argv[3]);
-        } else {
-          cperror(RED, "Error: insufficient arguments for -i\n");
-          print_help(argv[0]);
-        }
-        break;
-      case 'h': // -h or --help
-        print_help(argv[0]);
-        break;
-      case 'r':
-        run_proot_container(argv[2]);
-        break;
-      case 't': // -t or --test
-        // 测试选项的处理
-        break;
-      default:
-        cprintf(RED, "Error: unknown option \"%s\"\n", option);
-        print_help(argv[0]);
-      }
+        fprintf(stderr, "Error: insufficient arguments for install\n");
+        handle_help(argc, argv);
     }
-  } else {
-    cprintf(RED, "Error: unknown option \"%s\"\n", option);
-    print_help(argv[0]);
-  }
+}
+
+// 处理帮助命令
+void handle_help(int argc, char *argv[]) {
+(void)argc;
+    printf("Usage: %s [options] program [arg...]\n", argv[0]);
+    printf("Options:\n");
+    for (size_t i = 0; options[i].short_opt != NULL; i++) {
+        printf("  %s, %s\t%s\n", 
+               options[i].short_opt, 
+               options[i].long_opt, 
+               options[i].description);
+    }
+}
+
+// 处理运行命令
+void handle_run(int argc, char *argv[]) {
+    if (argc >= 3) {
+        run_proot_container(argv[2]);
+    } else {
+        fprintf(stderr, "Error: insufficient arguments for run\n");
+        handle_help(argc, argv);
+    }
+}
+
+// 解析命令行参数并分发
+void dispatch_arguments(int argc, char *argv[]) {
+    if (argc < 2) {
+        handle_help(argc, argv);
+        return;
+    }
+
+    const char *option = argv[1];
+    for (size_t i = 0; options[i].short_opt != NULL; i++) {
+        if (strcmp(option, options[i].short_opt) == 0 || 
+            strcmp(option, options[i].long_opt) == 0) {
+            options[i].handler(argc, argv);
+            return;
+        }
+    }
+
+    fprintf(stderr, "Error: unknown option \"%s\"\n", option);
+    handle_help(argc, argv);
 }
 
 int main(int argc, char *argv[]) {
-  // 检查环境变量
-  config_init();
+    // 初始化配置
+    config_init();
 
-  unsetenv("LD_PRELOAD");
-#ifdef DEBUG
-  printf("HOME: %s\n", getenv("HOME"));
-  printf("PREFIX: %s\n", getenv("PREFIX"));
-  printf("\nPid:[%d].", getpid());
-  printf("\nUid:[%d].\n", getuid());
-#endif
-  dispatch_arguments(argc, argv);
-  config_cleanup();
-  return 0;
+    // 分发命令行参数
+    dispatch_arguments(argc, argv);
+
+    // 清理配置
+    config_cleanup();
+    return 0;
 }
